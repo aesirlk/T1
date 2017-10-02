af826e0d86c9fed03469c3003568672e771b4e21
Loki <lvmaesir@gmail.com>
2017-10-02 06:35:34 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int k;
+    double x_de[500];
+    double f_de_x;
+    double derivada_de_f_de_x;
+    int limite;
+    double tolerancia;
+    double h = 0.00001;
+    char funcao_escolhida;
+
+    printf("\t seja bem vindo \n");
+    printf("selecione sua equacao:");
+    printf("1 - ")
+
+
+    return 0;
+}
